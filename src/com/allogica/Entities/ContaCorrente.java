3dc4c592a1f1e6cb1d34236eed050f953f44a75f
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-05-20 14:12:02 -0300

    Commit de inicio de projeto

---
+++ src/com/allogica/Entities/ContaCorrente.java
@@ -0,0 +1,13 @@
+package com.allogica.Entities;
+
+public class ContaCorrente extends ContaIMPL{
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+    }
+}
---
+++ src/com/allogica/Entities/ContaPoupanca.java
@@ -0,0 +1,13 @@
+package com.allogica.Entities;
+
+public class ContaPoupanca extends ContaIMPL{
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+    }
+}
