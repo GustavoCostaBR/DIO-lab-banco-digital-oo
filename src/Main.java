9dade7135dd7247bffeda9bc5cc2bfd09f73ef5c
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-05-21 11:03:20 -0300

    Main atualizado

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import com.allogica.Entities.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Banco santander = new Banco("Santander");
+        Cliente gustavo = new Cliente();
+        gustavo.setNome("Gustavo");
+
+        Conta cc = new ContaCorrente(gustavo, santander);
+        Conta poupanca = new ContaPoupanca(gustavo, santander);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+
+        santander.imprimirListaContasInfoPrivadas();
+        santander.imprimirListaContasInfoPublicas();
     }
 }
